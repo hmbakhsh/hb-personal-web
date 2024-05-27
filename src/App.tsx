cd53be85e12aedd3e75da7d01f52f3cc63ee93e0
Haroon Bakhsh <hmbakhsh01@gmail.com>
2024-05-27 12:53:53 +0100

    added basic styling to site

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import "./index.css";
 import "./App.css";
 
 function App() {
 	return (
 		<>
-			<h1 className="text-3xl font-bold underline">haroon bakhsh</h1>
-			<p>h@hbak.co</p>
+			<div className="flex min-h-screen justify-center items-center">
+				<div className="header">
+					<h1 className="text-5xl font-bold text-white">haroon bakhsh</h1>
+					<a href="mailto:h@hbak.co" className="text-indigo-300 mr-2 hover:text-indigo-100">
+						h@hbak.co
+					</a>
+					<a href="https://x.com/hmbakhsh" className="text-indigo-300 mr-2 hover:text-indigo-100">
+						x/@hmbakhsh
+					</a>
+					<a href="https://github.com/hmbakhsh" className="text-indigo-300 hover:text-indigo-100">
+						github/@hmbakhsh
+					</a>
+				</div>
+			</div>
 		</>
 	);
 }
