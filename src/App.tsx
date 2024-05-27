2deb9287bc3dfd176971525a8c596af1b421c528
Haroon Bakhsh <hmbakhsh01@gmail.com>
2024-05-27 19:17:05 +0100

    more media queries for responsive mobile design

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
 		<>
 			<div className="flex min-h-screen max-h-full justify-center items-center">
 				<div className="header">
-					<h1 className="text-2xl lg:text-7xl font-bold text-white mb-4">haroon bakhsh</h1>
-					<div className="flex">
+					<h1 className="text-2xl md:text-4xl lg:text-7xl font-bold text-white mb-4">haroon bakhsh</h1>
+					<div className="flex flex-col md:flex-row">
 						<Link link="mailto:h@hbak.co" content="h@hbak.co" />
 						<Link link="https://github.com/hmbakhsh" content="github/hmbakhsh" />
 						<Link link="https://x.com/hmbakhsh" content="x/hmbakhsh" />
@@ -20,3 +20,4 @@ function App() {
 }
 
 export default App;
+2;
--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -5,7 +5,7 @@ interface LinkProps {
 
 export const Link = ({ link, content }: LinkProps) => {
 	return (
-		<a href={link} className="text-4xl text-indigo-300 mr-6 hover:text-indigo-100">
+		<a href={link} className="text-lg md:text-2xl lg:text-4xl text-indigo-300 hover:text-indigo-100 md:mr-6">
 			{content}
 		</a>
 	);
