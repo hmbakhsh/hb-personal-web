843ee53732954e40771f80151b6cbef1301e09f4
Haroon Bakhsh <hmbakhsh01@gmail.com>
2024-05-27 13:10:26 +0100

    adjusted font sizing & content positioning

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import "./App.css";
 function App() {
 	return (
 		<>
-			<div className="flex min-h-screen justify-center items-center">
+			<div className="flex min-h-screen max-h-full justify-center items-center">
 				<div className="header">
-					<h1 className="text-5xl font-bold text-white">haroon bakhsh</h1>
-					<a href="mailto:h@hbak.co" className="text-indigo-300 mr-2 hover:text-indigo-100">
+					<h1 className="text-8xl font-bold text-white">haroon bakhsh</h1>
+					<a href="mailto:h@hbak.co" className="text-4xl text-indigo-300 mr-6 hover:text-indigo-100">
 						h@hbak.co
 					</a>
-					<a href="https://x.com/hmbakhsh" className="text-indigo-300 mr-2 hover:text-indigo-100">
+					<a href="https://x.com/hmbakhsh" className="text-4xl text-indigo-300 mr-6 hover:text-indigo-100">
 						x/@hmbakhsh
 					</a>
-					<a href="https://github.com/hmbakhsh" className="text-indigo-300 hover:text-indigo-100">
+					<a href="https://github.com/hmbakhsh" className="text-4xl text-indigo-300 hover:text-indigo-100">
 						github/@hmbakhsh
 					</a>
 				</div>
